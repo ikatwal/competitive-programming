75b73d0db5cc4573628e4ea13ae071954da34d33
ikatwal <teglanigiro@gmail.com>
2023-04-03 17:49:54 -0400

    sovled 235

---
+++ lca_bst/least_common_ancestor.go
@@ -0,0 +1,26 @@
+package main
+
+// 235. Lowest Common Ancestor of a Binary Search Tree
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+
+	return root
+
+}
